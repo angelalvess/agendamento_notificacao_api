9372f6cb6af40bb483f02fef9aa93a6b4106ec20
angelalves <angeltrashangel@gmail.com>
2025-02-28 04:52:43 -0300

    fix to annotations problems on AgendamentoController and AgendamentoService

--- src/main/java/com/angel/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/angel/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -1,20 +1,20 @@
 package com.angel.agendamento_notificacao_api.business.mapper;
 
-
 import com.angel.agendamento_notificacao_api.controller.dto.in.AgendamentoRecord;
 import com.angel.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut;
 import com.angel.agendamento_notificacao_api.infrastructure.entities.Agendamento;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
 
 @Mapper(componentModel = SPRING)
 public interface IAgendamentoMapper {
 
-    Agendamento paraEntity(AgendamentoRecord agendamento);
-    AgendamentoRecordOut paraOut(Agendamento agendamento);
+    Agendamento paraEntity (AgendamentoRecord agendamento);
+
+    AgendamentoRecordOut paraOut (Agendamento agendamento);
 
     @Mapping(target = "dataHoraModificado", expression = "java(LocalDateTime.now())")
     @Mapping(target = "statusNotificacao", expression = "java(StatusNotificacaoEnum.CANCELADO)")
